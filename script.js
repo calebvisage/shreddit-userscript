1ce60591078913e066f8cd68d25920fc615b533f
calebvisage <68855603+calebvisage@users.noreply.github.com>
2023-11-06 13:04:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name        Shreddit - reddit.com
+// @namespace   Violentmonkey Scripts
+// @match       https://www.reddit.com/*
+// @grant       none
+// @version     1.0
+// @author      https://github.com/calebvisage
+// @description 2023/11/06, 13:01:25
+// ==/UserScript==
+
 const removeElement = () => {
   const element = document.querySelector("shreddit-async-loader.theme-beta");
   if (element) {
